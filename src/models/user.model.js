c60d6acb71366ace014a312de34bada41021bc4b
alihossen4 <mdahossen.dev@gmail.com>
2026-09-20 20:12:53 +0600

    feat: model updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,6 +2,11 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import db from "../db/index.js";
 const userSchema = new Schema({
+    email:{
+        type:String,
+        required: true,
+        unique: true,
+    },
     firtname: {
         type:String,
         required: true,
@@ -10,19 +15,16 @@ const userSchema = new Schema({
         type:String,
         required: true,
     },
-    email:{
-        type:String,
+    mobile:{
+        type: String,
         required: true,
     },
     password: {
         type: String,
         required: true,
     },
-    mobile:{
-        type: String,
-        required: true,
-    },
 });
+
 userSchema.pre('save', async function(next){
     if(!this.isModified('password') ||!this.password) return next();
     this.password = await bcrypt.hash(this.password, 10);
@@ -33,4 +35,4 @@ userSchema.methods.comparePassword = await async function(password){
     return await bcrypt.compare(password, this.password);
 }
 
-export const User = mongoose.model.User || db.model("User", userSchema);
+export const User = mongoose.model.User || mongoose.model("User", userSchema);
